c52be0b9cf296693e7680d800153a75a67a26896
Himanshu <86726677+HimanshuSRTOp@users.noreply.github.com>
2021-12-07 13:58:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ bot = commands.AutoShardedBot(command_prefix="~")
 async def on_ready():
   print("Hello world")
   
-bot.run("OTE3NjkzNzA0OTc2ODc1NTUw.Ya8a3Q.UZ7wgO6vMyOdsGQe_MGvae0SMao")
+bot.run("TOKEN HERE")
