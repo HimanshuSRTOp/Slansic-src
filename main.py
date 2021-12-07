099a82610d942c746bfca3c970a6dace009f1464
HimanshuDHTOp <raj2253sharma@gmail.com>
2021-12-07 14:04:53 +0530

    src

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-import discord
-from discord.ext import commands
-
-bot = commands.AutoShardedBot(command_prefix="~")
-
-@bot.event
-async def on_ready():
-  print("Hello world")
-  
-bot.run("TOKEN HERE")
+# THIS FILE HAS BEEN SHIFTED TO COGS
+# never gonna give you.
